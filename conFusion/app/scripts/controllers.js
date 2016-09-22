4eb0c0fa2a31d833c9d09bd9aec67f987119af2b
David Gudeman <davidmgudeman@gmail.com>
2016-09-22 12:49:37 -0700

    Week 03 - Exercise 3 completed

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -31,8 +31,8 @@ angular.module('confusionApp')
         };
 
         //$scope.isSelected = function (checkTab) {
-         //   return ($scope.tab === checkTab);
-       // };
+        //   return ($scope.tab === checkTab);
+        // };
     }])
     .controller('ContactController', ['$scope', function ($scope) {
 
@@ -42,7 +42,6 @@ angular.module('confusionApp')
         $scope.channels = channels;
         $scope.invalidChannelSelection = false;
     }])
-
 
     .controller('FeedbackController', ['$scope', function ($scope) {
         $scope.sendFeedback = function () {
@@ -64,56 +63,13 @@ angular.module('confusionApp')
             }
         };
     }])
-    .controller('DishDetailController', ['$scope', function ($scope) {
 
-        var dish = {
-            name: 'Uthapizza',
-            image: 'images/uthapizza.png',
-            category: 'mains',
-            label: 'Hot',
-            price: '4.99',
-            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comments: [
-                {
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
-                },
-                {
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
-                }
-
-            ]
-        };
-
+    .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function ($scope, $routeParams, menuFactory) {
+        var dish = menuFactory.getDish(parseInt($routeParams.id,10));
         $scope.dish = dish;
-        this.orderBy = '';
     }])
 
     .controller('DishCommentController', ['$scope', function ($scope) {
-
         //Step 1: Create a JavaScript object to hold the comment from the form
         $scope.comment = { rating: 5, comment: "", author: "" };
 
